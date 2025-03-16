3b43053a993865b3233bf14cc2f3fb72b411fc06
Yoiner Diaz <yoinerdr@gmail.com>
2025-03-15 23:05:50 -0500

    fist commit

---
+++ src/main/java/com/innovatexts/mi_finquita/MiFinquitaApplication.java
@@ -0,0 +1,13 @@
+package com.innovatexts.mi_finquita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiFinquitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiFinquitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innovatexts/mi_finquita/MiFinquitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovatexts.mi_finquita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiFinquitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
